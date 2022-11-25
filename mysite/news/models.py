8c6796ccb763567f666bd17901948c048716dfb0
alongkornubu <alongkorn.ch.62@ubu.ac.th>
2022-11-25 22:25:36 +0700

    edit models

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -6,10 +6,18 @@ class Feedback(models.Model):
 	link = models.CharField(max_length = 200)
 	detail = models.TextField(blank=True , null= True)
 
+chocies_type = [
+	('covid-19','โควิด-19'),
+	('hemp','กัญชา'),
+	('bank','ธนาคาร'),
+]
+
 class AddNew(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.CharField(max_length=500)	
-	tag = models.CharField(max_length=100)
+	tag = models.CharField(max_length=50 ,choices=chocies_type, default='covid-19')
 	image = models.ImageField(upload_to='images', blank=True, null = True) 
+
+	
 
 
